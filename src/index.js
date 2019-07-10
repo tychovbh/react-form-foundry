05c6abc0e904d0d7bf49a35bce040ac86cf6e51e
Tycho van Berge Henegouwen <info@bespokeweb.nl>
2019-07-10 12:45:16 +0200

    refactor class to hooks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
 const DefaultInput = (props) => <input {...props}/>
 const DefaultTextarea = (props) => <textarea {...props}/>
@@ -52,46 +52,44 @@ const SelectField = ({field, component, onChange, id, state}) => {
     </Component>
 }
 
-export default class Form extends Component {
-    constructor(props) {
-        super(props)
+const Fields = {
+    input: InputField,
+    textarea: TextAreaField,
+    select: SelectField,
+}
 
-        let model = {}
-        const form = props.form
+const fields = (form, defaults) => {
+    const fields = {}
+    form.fields.forEach((field) => {
+        fields[field.properties.name] = ''
+        if (field.element.name === 'select' && field.properties.options.length) {
+            fields[field.properties.name] = field.properties.options[0].value
+        }
+        if (field.properties.default) {
+            fields[field.properties.name] = field.properties.default
+        }
+        if (defaults && defaults[field.properties.name]) {
+            fields[field.properties.name] = defaults[field.properties.name]
+        }
+    })
 
-        form.fields.forEach((field) => {
-            model[field.properties.name] = ''
-            if (field.element.name === 'select' && field.properties.options.length) {
-                model[field.properties.name] = field.properties.options[0].value
-            }
-            if (field.properties.default) {
-                model[field.properties.name] = field.properties.default
-            }
-            if (props.defaults && props.defaults[field.properties.name]) {
-                model[field.properties.name] = props.defaults[field.properties.name]
-            }
-        })
+    return fields
+}
 
-        this.state = {model, form}
-    }
+export default ({form, onSubmit, onResponse, components, defaults}) => {
+    const [model, setModel] = useState(fields(form, defaults))
 
-    fields = {
-        input: InputField,
-        textarea: TextAreaField,
-        select: SelectField,
-    }
-
-    submit = (event) => {
+    const submit = (event) => {
         event.preventDefault()
-        const {model} = this.state
-        const {onResponse} = this.props
         let formData = new FormData()
 
         for (let name in model) {
-            formData.append(name, model[name])
+            if (model.hasOwnProperty(name)) {
+                formData.append(name, model[name])
+            }
         }
 
-        fetch(this.state.form.route, {
+        fetch(form.route, {
             method: 'post',
             body: formData
         }).then((response) => {
@@ -101,107 +99,45 @@ export default class Form extends Component {
         })
     }
 
-    component(name) {
-        const {components} = this.props
+    const component = (name) => {
         return components && components[name] ? components[name] : null
     }
 
-    render() {
-        const {model, form} = this.state
-        const {onSubmit} = this.props
-        const Submit = this.component('submit') || DefaultInput
-        const Label = this.component('label') || DefaultLabel
-        const Title = this.component('title') || DefaultTitle
-        const Description = this.component('description') || DefaultDescription
+    const Submit = component('submit') || DefaultInput
+    const Label = component('label') || DefaultLabel
+    const Title = component('title') || DefaultTitle
+    const Description = component('description') || DefaultDescription
 
-        return (
-            <form onSubmit={onSubmit ? (event) => onSubmit(event, model) : this.submit}>
-                {form.label && <Title>{form.label}</Title>}
-                {form.description && <Description>{form.description}</Description>}
-                {
-                    form.fields.map((field, index) => {
-                        const Field = this.fields[field.element.name]
-                        const id = `form-generator-field-${field.properties.name}`
+    return (
+        <form onSubmit={onSubmit ? (event) => onSubmit(event, model) : submit}>
+            {form.label && <Title>{form.label}</Title>}
+            {form.description && <Description>{form.description}</Description>}
+            {
+                form.fields.map((field, index) => {
+                    const Field = Fields[field.element.name]
+                    const id = `form-generator-field-${field.properties.name}`
 
-                        return (
-                            <div className={'form-generator-field'} key={index}>
-                                {
-                                    field.properties.label &&
-                                    <Label htmlFor={id}>
-                                        {field.properties.label}
-                                    </Label>
-                                }
-                                <Field
-                                    id={id}
-                                    field={field}
-                                    state={model[field.properties.name]}
-                                    component={this.component(field.element.name)}
-                                    onChange={(value) => {
-                                        model[field.properties.name] = value
-                                        this.setState({model})
-                                    }}/>
-                            </div>
-                        )
-                    })
-                }
-                <Submit type={'submit'} value={'Submit'}/>
-            </form>
-        )
-    }
+                    return (
+                        <div className={'form-generator-field'} key={index}>
+                            {
+                                field.properties.label &&
+                                <Label htmlFor={id}>
+                                    {field.properties.label}
+                                </Label>
+                            }
+                            <Field
+                                id={id}
+                                field={field}
+                                state={model[field.properties.name]}
+                                component={component(field.element.name)}
+                                onChange={(value) => {
+                                    setModel({...model, [field.properties.name]: value})
+                                }}/>
+                        </div>
+                    )
+                })
+            }
+            <Submit type={'submit'} value={'Submit'}/>
+        </form>
+    )
 }
-
-// const Fields = {
-//     text: TextField,
-//     number: NumberField,
-//     textarea: TextAreaField,
-//     file: FileField,
-// }
-//
-// export default ({form}) => {
-//
-//     const [model, setModel] = useState({})
-//
-//     /*  const handleSubmit = (event) => {
-//           event.preventDefault()
-//           const {model} = this.state
-//
-//           let formData  = new FormData()
-//
-//           for (let name in model) {
-//               formData.append(name, model[name])
-//           }
-//
-//           fetch(form.route, {
-//               method: 'post',
-//               body:  formData
-//           }).then((response) => {
-//               //
-//           });
-//       }*/
-//
-//
-//     return (
-//         <div>
-//             <form onSubmit={handleSubmit}>
-//                 <h3>Titel: {form.label}</h3>
-//                 <p>Omschrijving: {form.description}</p>
-//                 {
-//                     form.fields.map((field, index) => {
-//                         const Field = this.fields[field.input.name]
-//
-//                         return (
-//                             <label key={index}>
-//                                 {field.label}
-//                                 <Field onChange={(value) => {
-//                                     model[field.name] = value
-//                                     setModel(model)
-//                                 }} placeholder={field.placeholder}/>
-//                             </label>
-//                         )
-//                     })
-//                 }
-//                 <input type={'submit'} value={'Submit'}/>
-//             </form>
-//         </div>
-//     )
-// }
